3b8c458e9e6216ec7c669fe3bb7bea8612899ef0
KritantaDev <KritantaDevelopment@gmail.com>
2021-10-06 16:23:10 -0400

    Add setup script

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='corellium-toolkit',
+      version='0.1.0',
+      description='Corellium Tookit',
+      long_description='file: README.md',
+      long_description_content_type='text/markdown',
+      author='kat',
+      url='https://github.com/kritantadev/corellium-toolkit',
+      install_requires=['requests'],
+      packages=['kcorellium'],
+      package_dir={
+            'kcorellium': 'src/kcorellium',
+      },
+      classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent'
+      ],
+      scripts=['bin/cachebuilder']
+      )
